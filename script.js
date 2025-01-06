fb3902f6494bb381d852d3c0ae928ab06d0d50e3
Dylan Fitch <kkarl30@gmail.com>
2025-01-06 12:09:12 -0500

    wrote function for display books stored in myLibrary

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function Book(author, title, pages, isRead){
             return bookInfo + 'is not read';
         }
 
-        return 
+        
     }
 }
 
@@ -30,6 +30,30 @@ function addBookToLibrary(author, title, pages, isRead){
 }
 
 
+function displayBooks(){
+
+    let booksContainer = document.querySelector(".books");
+
+    for(let i = 0; i < myLibrary.length; i++){
+
+        let newBook = document.createElement("div");
+        newBook.setAttribute("class", "book");
+
+        for(const property in myLibrary[i]){
+            if(myLibrary[i].hasOwnProperty(property) && typeof myLibrary[i][property] !== "function"){
+                let textDiv = document.createElement("div");
+                textDiv.textContent = `${property}: ${myLibrary[i][property]}`;
+                newBook.appendChild(textDiv);
+            }
+        }
+        
+        booksContainer.appendChild(newBook);
+    }
+
+
+}
+
+
 addBookToLibrary("J.R.R Tolkien", "The Hobbit", 295, true);
 addBookToLibrary("J.R.R Tolkien", "The Followship of the Ring", 395, true);
 addBookToLibrary("J.R.R Tolkien", "The Two Towers", 395, true);
@@ -37,3 +61,6 @@ addBookToLibrary("J.R.R Tolkien", "The Two Towers", 395, true);
 for(let i = 0; i < myLibrary.length; i++){
     console.log(myLibrary[i].info());
 }
+
+
+displayBooks();
